6a9f1290633b15603c4a4313b6d65134e4c17af8
Baby Chin <352484005@qq.com>
2020-12-17 11:14:10 +0800

    修改配置

--- server/index.js
+++ server/index.js
@@ -22,7 +22,6 @@ async function start() {
 
   app.use(ctx => {
     ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-
     return new Promise((resolve, reject) => {
       ctx.res.on('close', resolve)
       ctx.res.on('finish', resolve)
